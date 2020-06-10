0604b088935e546e3aa8f4909f9c0714a63b9789
NoPMK <gacsdan@gmail.com>
2020-06-10 19:21:52 +0200

    Changed

---
+++ script.js
@@ -0,0 +1,16 @@
+let names = ["Robert" , "Elias" , "Sandy" , "Herbert" , "Daniel"];
+
+
+
+names.forEach((name) => {
+   $("li").append(`${name}!`);
+  })
+
+
+    let newI = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+  };
+  
+  $(".ntitle").html("Added with javascript")
+  $(".ntext").html("This block was added using JavaScript's jQuery library. How awesome!")
